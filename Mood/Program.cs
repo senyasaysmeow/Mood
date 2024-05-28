a2442b429e235050d93f6a26452b3eec9b944686
senyasaysmeow <arsenv2005@gmail.com>
2024-05-28 12:38:48 +0300

    pics :)

--- Mood/Program.cs
+++ Mood/Program.cs
@@ -3,6 +3,7 @@ using System.Threading;
 using System.Threading.Tasks;
 using Telegram.Bot;
 using Telegram.Bot.Types;
+using Telegram.Bot.Types.ReplyMarkups;
 
 namespace Mood
 {
@@ -20,27 +21,36 @@ namespace Mood
         async static Task Update(ITelegramBotClient botClient, Update update, CancellationToken token, SpotifyClient sp)
         {
             var message = update.Message;
-            if (message.Text != null)
+            if (message?.Text != null)
             {
                 if (message.Text.ToLower() == "/start")
                 {
-                    await botClient.SendTextMessageAsync(message.Chat.Id, $"To authenticate with Spotify click the link below:\n{sp.getLink()}");
+                    var sentMassage = await botClient.SendTextMessageAsync(message.Chat.Id, $"Вітаю! \nЩоб авторизуватися, перейдіть за посиланням нижче:\n{sp.getLink()}");
                     await sp.AUTH();
-                    await botClient.SendTextMessageAsync(message.Chat.Id, $"Authorization Successful!");
-                    await botClient.SendTextMessageAsync(message.Chat.Id, $"Now describe your mood on a scale from 0.0 to 1.0 :)");
+                    await botClient.DeleteMessageAsync(message.Chat.Id, sentMassage.MessageId);
+                    await botClient.SendTextMessageAsync(message.Chat.Id, $"Успішно авторизовано!");
+                    await botClient.SendPhotoAsync(
+                        chatId: message.Chat.Id,
+                        photo: InputFile.FromUri("https://i.pinimg.com/564x/48/1d/c7/481dc7e0c20835bcb3fc9abcd921b27b.jpg"), 
+                        caption: "Тепер опишіть свій настрій від 0.0 до 1.0 :)"
+                    );
                 }
                 else if (float.TryParse(message.Text, out float mood) && mood >= 0.0 && mood <= 1.0)
                 {
-                    var sentMassage = await botClient.SendTextMessageAsync(message.Chat.Id, $"Creating your playlist...");
+                    var sentMassage = await botClient.SendTextMessageAsync(message.Chat.Id, $"Готуємо плейлист для вас...");
                     var tracks = await sp.GetMoodPlaylist(mood);
                     var pl = await sp.CreatePlaylist($"mood {mood}");
                     await sp.AddTracksToPlaylist(pl, tracks);
                     await botClient.DeleteMessageAsync(message.Chat.Id, sentMassage.MessageId);
-                    await botClient.SendTextMessageAsync(message.Chat.Id, $"Playlist created based on your mood:\nhttps://open.spotify.com/playlist/{pl}");
+                    await botClient.SendPhotoAsync(
+                        chatId: message.Chat.Id,
+                        photo: InputFile.FromUri("https://i.pinimg.com/564x/8b/eb/5a/8beb5ade3368528534e1ff6cd9d50724.jpg"), 
+                        caption: $"Ось плейлист підібраний спеціально під ваш настрій:\nhttps://open.spotify.com/playlist/{pl}"
+                    );
                 }
                 else if (message.Text.ToLower() == "/restart")
                 {
-                    await botClient.SendTextMessageAsync(message.Chat.Id, $"If you want to create another mood playlist just describe your mood again on a scale from 0.0 to 1.0 :)");
+                    await botClient.SendTextMessageAsync(message.Chat.Id, "Якщо ви хочете створити ще один плейлист за настроєм просто опишіть цей настрій від 0.0 до 1.0 :)");
                 }
             }
         }
